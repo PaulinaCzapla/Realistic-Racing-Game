02e75a563a9546823dda45d47afb1325f40a3e95
swietjak <japaaapa25@gmail.com>
2022-04-04 21:49:13 +0200

    feat: initial room scene added

--- Assets/Scripts/UI/MainMenu/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuController.cs
@@ -9,16 +9,26 @@ namespace UI.MainMenu
     public class MainMenuController : MonoBehaviour
     {
         [SerializeField] private Button startButton;
+        [SerializeField] private Button roomButton;
         [SerializeField] private LoadSceneEventChannelSO loadSceneEvent;
         [SerializeField] private GameSceneSO RaceTrackScene;
+        [SerializeField] private GameSceneSO JoinRoomScene;
+
+        private void JoinRoomEvent()
+        {
+            loadSceneEvent.RaiseEvent(JoinRoomScene, true);
+        }
+
         private void OnEnable()
         {
             startButton.onClick.AddListener(() => loadSceneEvent.RaiseEvent(RaceTrackScene, true));
+            roomButton.onClick.AddListener(() => JoinRoomEvent());
         }
 
         private void OnDisable()
         {
             startButton.onClick.RemoveAllListeners();
+            roomButton.onClick.RemoveAllListeners();
         }
     }
 }
