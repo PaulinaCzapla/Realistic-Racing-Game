fc7f5e14d388f791d8b24fbd48a2f54b954d87b0
Paulina Czapla <paulina.czp00@gmail.com>
2022-04-20 21:35:09 +0200

    Implemented ackermann steering

--- Assets/Scripts/Car/WheelsManagement/Wheel.cs
+++ Assets/Scripts/Car/WheelsManagement/Wheel.cs
@@ -12,11 +12,8 @@ namespace Car.WheelsManagement
         
         //temporary place for const values - will be moved to scriptable object
         private const float RotationSpeedMultiplier = 0.5f;
-        private const float MAXWheelsRotationAngle = 30f;
         private float _initialAngle;
         private Transform _transform;
-        
-        
 
         private void Awake()
         {
@@ -32,19 +29,6 @@ namespace Car.WheelsManagement
         public void ApplyBrakeTorque(float force)
         {
             collider.brakeTorque = force;
-        }
-        public void RotateWheel(float direction)
-        {
-            if (Mathf.Approximately(direction, 0) && !Mathf.Approximately(collider.steerAngle, _initialAngle))
-            {
-                //if there is no input in x axis - slowly center the wheels
-                HandleAutoCenteringWheels();
-            }
-            else if (Mathf.Abs(collider.steerAngle) <= MAXWheelsRotationAngle || (collider.steerAngle * -direction) >= 0)
-            {
-                collider.steerAngle += direction * RotationSpeedMultiplier;
-            }
-           
         }
         
         public void UpdateWheel()
@@ -66,6 +50,11 @@ namespace Car.WheelsManagement
             {
                 collider.steerAngle = _initialAngle;
             }
+        }
+
+        public void SetSteeringAngle(float angle)
+        {
+            collider.steerAngle = angle;
         }
     }
 }
--- Assets/Scripts/Utilities/AckermannUtility.cs
+++ Assets/Scripts/Utilities/AckermannUtility.cs
@@ -1,7 +1,16 @@
-﻿namespace Utilities
+﻿using UnityEngine;
+
+namespace Utilities
 {
-    public class AckermannUtility
+    public static class AckermannUtility
     {
-        
+        public static float GetSecondaryAngle(float primaryAngle, float separation, float width) 
+        {
+            if (Mathf.Abs(primaryAngle) < 1)
+                primaryAngle = Mathf.Abs(primaryAngle);
+            float close = separation / Mathf.Tan(Mathf.Abs(primaryAngle) * Mathf.Deg2Rad);
+            float far = close + width;
+            return Mathf.Sign(primaryAngle) * Mathf.Atan(separation / far) * Mathf.Rad2Deg;
+        }
     }
 }
