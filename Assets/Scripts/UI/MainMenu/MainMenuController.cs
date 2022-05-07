dbe2cb5388819705d8f958009ac9b5357f6e5297
swietjak <japaaapa25@gmail.com>
2022-05-07 19:53:29 +0200

    fix: menu fix

--- Assets/Scripts/UI/MainMenu/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuController.cs
@@ -10,20 +10,57 @@ namespace UI.MainMenu
     {
         [SerializeField] private Button startButton;
         [SerializeField] private Button roomButton;
+        [SerializeField] private Button optionsButton;
+        [SerializeField] private Button creditsButton;
+        [SerializeField] private Button backButton;
         [SerializeField] private LoadSceneEventChannelSO loadSceneEvent;
         [SerializeField] private GameSceneSO RaceTrackScene;
         [SerializeField] private GameSceneSO JoinRoomScene;
+        [SerializeField] private GameObject OptionsContainer;
+        [SerializeField] private GameObject CreditsContainer;
+        [SerializeField] private GameObject MenuContainer;
 
+        private void OnStart()
+        {
+            backButton.gameObject.SetActive(false);
+        }
         private void OnEnable()
         {
             startButton.onClick.AddListener(() => loadSceneEvent.RaiseEvent(RaceTrackScene, true));
             roomButton.onClick.AddListener(() => loadSceneEvent.RaiseEvent(JoinRoomScene, true));
+            optionsButton.onClick.AddListener(() => handleOptionsButtonClick());
+            creditsButton.onClick.AddListener(() => handleCreditsButtonClick());
+            backButton.onClick.AddListener(() => handleBackButtonClick());
+        }
+
+        private void handleBackButtonClick()
+        {
+            OptionsContainer.SetActive(false);
+            CreditsContainer.SetActive(false);
+            MenuContainer.SetActive(true);
+            backButton.gameObject.SetActive(false);
+        }
+        private void handleOptionsButtonClick()
+        {
+            OptionsContainer.SetActive(true);
+            MenuContainer.SetActive(false);
+            backButton.gameObject.SetActive(true);
+        }
+
+        private void handleCreditsButtonClick()
+        {
+            CreditsContainer.SetActive(true);
+            MenuContainer.SetActive(false);
+            backButton.gameObject.SetActive(true);
         }
 
         private void OnDisable()
         {
             startButton.onClick.RemoveAllListeners();
             roomButton.onClick.RemoveAllListeners();
+            optionsButton.onClick.RemoveAllListeners();
+            creditsButton.onClick.RemoveAllListeners();
+            backButton.onClick.RemoveAllListeners();
         }
     }
 }
