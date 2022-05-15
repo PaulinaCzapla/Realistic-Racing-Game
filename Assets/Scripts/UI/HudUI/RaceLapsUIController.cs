4b15c970b3b631a47077cfd95c8be5e9ae110457
AgataFrancik <agata.francik@interia.pl>
2022-05-15 21:14:08 +0200

    tahometer

--- Assets/Scripts/UI/HudUI/RaceLapsUIController.cs
+++ Assets/Scripts/UI/HudUI/RaceLapsUIController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Events.ScriptableObjects;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 namespace UI.HudUI
 {
@@ -11,8 +13,50 @@ namespace UI.HudUI
         [SerializeField] private VoidEventChannelSO onRaceFinished;
         [SerializeField] private IntEventChannelSO onUpdateLapsCount;
 
+        [SerializeField] int UpdateFrameCount = 3;
+	    [SerializeField] TextMeshProUGUI SpeedText;
+	    [SerializeField] TextMeshProUGUI CurrentGearText;
+
+	    [SerializeField] RectTransform TahometerArrow;
+	    [SerializeField] float MinArrowAngle = 0;
+	    [SerializeField] float MaxArrowAngle = -315f;
+
+        int CurrentFrame;
+
+        [SerializeField] private CarSO car;
+
+        CarSO SelectedCar { get { return GameController.PlayerCar; } }
+
         private int _maxLapsCount;
         private int _lapsCount;
+
+        private void Update ()
+	{
+
+		if (CurrentFrame >= UpdateFrameCount)
+		{
+			UpdateGamePanel ();
+			CurrentFrame = 0;
+		}
+		else
+		{
+			CurrentFrame++;
+		}
+
+		UpdateArrow ();
+	}
+
+        void UpdateArrow ()
+	{
+		var procent = SelectedCar.EngineRPM / SelectedCar.GetMaxRPM;
+		var angle = (MaxArrowAngle - MinArrowAngle) * procent + MinArrowAngle;
+		TahometerArrow.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+	}
+    void UpdateGamePanel ()
+	{
+		SpeedText.text = SelectedCar.SpeedInHour.ToString ("000");
+		CurrentGearText.text = SelectedCar.CurrentGear.ToString ();
+	}
 
         private void OnEnable()
         {
