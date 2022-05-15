e68596e060d1da9269c7ae547ca183f9650ad7ae
Paulina Czapla <paulina.czp00@gmail.com>
2022-05-15 21:57:02 +0200

    Refactor

--- Assets/Scripts/CameraFollow/FollowingCamera.cs
+++ Assets/Scripts/CameraFollow/FollowingCamera.cs
@@ -9,8 +9,6 @@ namespace CameraFollow
         
         [SerializeField] private float distance = 10.0f;
         [SerializeField] private float height = 5.0f;
-        [SerializeField] private float rotationXOffset = 10f;
-
         [SerializeField] private float rotationDamping;
         [SerializeField] private float heightDamping;
         
@@ -20,31 +18,21 @@ namespace CameraFollow
                 return;
             var wantedRotationAngle = target.eulerAngles.y;
             var wantedHeight = target.position.y + height;
-
             var currentRotationAngle = transform.eulerAngles.y;
             var currentHeight = transform.position.y;
-
-            // Damp the rotation around the y-axis
+            
             currentRotationAngle =
                 Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
-
-            // Damp the height
+            
             currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
-
-            // Convert the angle into a rotation
+            
             var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
-
-            // Set the position of the camera on the x-z plane to:
-            // distance meters behind the target
+            
             transform.position = target.position;
             transform.position -= currentRotation * Vector3.forward * distance;
-
-            // Set the height of the camera
             transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
-
-            // Always look at the target
-            transform.LookAt(target);
             
+            transform.LookAt(target);
         }
     }
 }
