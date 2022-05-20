3cde07a60e6dbc0757a6b47e66f94b5c9b0f444e
Marta <marta.osm00@gmail.com>
2022-05-20 14:40:16 +0200

    basic tachometer

--- Assets/Scripts/UI/HudUI/RaceLapsUIController.cs
+++ Assets/Scripts/UI/HudUI/RaceLapsUIController.cs
@@ -8,55 +8,60 @@ namespace UI.HudUI
 {
     public class RaceLapsUIController : MonoBehaviour
     {
-        [Header("Event Channels")]
-        [SerializeField] private IntEventChannelSO onSetMaxLapsCount;
+        [Header("Event Channels")] [SerializeField]
+        private IntEventChannelSO onSetMaxLapsCount;
+
         [SerializeField] private VoidEventChannelSO onRaceFinished;
         [SerializeField] private IntEventChannelSO onUpdateLapsCount;
 
         [SerializeField] int UpdateFrameCount = 3;
-	    [SerializeField] TextMeshProUGUI SpeedText;
-	    [SerializeField] TextMeshProUGUI CurrentGearText;
+        [SerializeField] TextMeshProUGUI SpeedText;
+        [SerializeField] TextMeshProUGUI CurrentGearText;
 
-	    [SerializeField] RectTransform TahometerArrow;
-	    [SerializeField] float MinArrowAngle = 0;
-	    [SerializeField] float MaxArrowAngle = -315f;
+        [SerializeField] RectTransform TahometerArrow;
+        [SerializeField] float MinArrowAngle = 0;
+        [SerializeField] float MaxArrowAngle = -315f;
 
         int CurrentFrame;
 
         [SerializeField] private CarSO car;
 
-      //  CarSO SelectedCar { get { return GameController.PlayerCar; } }
+        //  CarSO SelectedCar { get { return GameController.PlayerCar; } }
 
         private int _maxLapsCount;
         private int _lapsCount;
 
-        private void Update ()
-	{
+        private void Update()
+        {
+            if (CurrentFrame >= UpdateFrameCount)
+            {
+                UpdateGamePanel();
+                CurrentFrame = 0;
+            }
+            else
+            {
+                CurrentFrame++;
+            }
 
-		if (CurrentFrame >= UpdateFrameCount)
-		{
-			UpdateGamePanel ();
-			CurrentFrame = 0;
-		}
-		else
-		{
-			CurrentFrame++;
-		}
+            UpdateArrow();
+        }
 
-		UpdateArrow ();
-	}
+        void UpdateArrow()
+        {
+            //var procent = car._engineRPM / car._maxRPM;
+            //var angle = (MaxArrowAngle - MinArrowAngle) * procent + MinArrowAngle;
+            //TahometerArrow.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            var procent = 0.3f *(car._engineRPM / car._maxRPM);
+            var angle = (MaxArrowAngle - MinArrowAngle) * procent + MinArrowAngle;
+            TahometerArrow.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            //TahometerArrow.transform.Rotate(0,0,angle);
+        }
 
-        void UpdateArrow ()
-	{
-		var procent = car._engineRPM / car._maxRPM;
-		var angle = (MaxArrowAngle - MinArrowAngle) * procent + MinArrowAngle;
-		TahometerArrow.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
-	}
-    void UpdateGamePanel ()
-	{
-		//SpeedText.text = car.{Tu jest miejsce na prędkość!}.ToString ("000");
-		CurrentGearText.text = car._gearNum.ToString ();
-	}
+        void UpdateGamePanel()
+        {
+            //SpeedText.text = car.{Tu jest miejsce na prędkość!}.ToString ("000");
+            CurrentGearText.text = car._gearNum.ToString();
+        }
 
         private void OnEnable()
         {
