86ffdb6f2dc065aef6d0900c56ce1db580656a35
Marta <marta.osm00@gmail.com>
2022-06-06 19:26:36 +0200

    some fixes

--- Assets/Scripts/Network/CreateAndJoinRooms.cs
+++ Assets/Scripts/Network/CreateAndJoinRooms.cs
@@ -1,59 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using Events.ScriptableObjects;
-using UnityEngine;
-using UnityEngine.UI;
 using Photon.Pun;
 using SceneManagement.ScriptableObjects;
-using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+namespace Network
 {
-    [SerializeField] private InputField createInput;
-    [SerializeField] private InputField joinInput;
-    [SerializeField] private Text connectionStatus;
+    public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+    {
+        [SerializeField] private InputField createInput;
+        [SerializeField] private InputField joinInput;
+        [SerializeField] private Text connectionStatus;
     
-    [SerializeField] private LoadSceneEventChannelSO photonLoadSceneEvent;
-    [SerializeField] private GameSceneSO colorLobbyScene;
+        [SerializeField] private LoadSceneEventChannelSO photonLoadSceneEvent;
+        [SerializeField] private GameSceneSO colorLobbyScene;
     
-    public void CreateRoom()
-    {
-        PhotonNetwork.CreateRoom(createInput.text);
-    }
-
-    public void JoinRoom()
-    {
-        PhotonNetwork.JoinRoom(joinInput.text);
-    }
-
-    public override void OnJoinedRoom()
-    {
-        connectionStatus.text = "Please wait...";
-        UnloadActiveScenes();
-        PhotonNetwork.LoadLevel("ColorLobby");
-        SceneManager.LoadSceneAsync("PersistentScene", LoadSceneMode.Additive);
-        //photonLoadSceneEvent.RaiseEvent();
-        //PhotonNetwork.LoadLevel("MultiplayerDemo");
-    }
-
-    public override void OnJoinRoomFailed(short returnCode, string message)
-    {
-        connectionStatus.text = message;
-    }
-
-    public override void OnCreateRoomFailed(short returnCode, string message)
-    {
-        connectionStatus.text = message;
-    }
-
-    private void UnloadActiveScenes()
-    {
-        var countLoaded = SceneManager.sceneCount;
-
-        for (var i = 0; i < countLoaded; i++)
+        public void CreateRoom()
         {
-            SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(i));
+            PhotonNetwork.CreateRoom(createInput.text);
         }
-        
+
+        public void JoinRoom()
+        {
+            PhotonNetwork.JoinRoom(joinInput.text);
+        }
+
+        public override void OnJoinedRoom()
+        {
+            connectionStatus.text = "Please wait...";
+            PhotonNetwork.LoadLevel("ColorLobby");
+        }
+
+        public override void OnJoinRoomFailed(short returnCode, string message)
+        {
+            connectionStatus.text = message;
+        }
+
+        public override void OnCreateRoomFailed(short returnCode, string message)
+        {
+            connectionStatus.text = message;
+        }
+    
     }
 }
